080c50ccaf17317d07e7a822ca00afeaa440e7d6
Climax85 <climax85@web.de>
2015-10-15 15:24:26 +0200

    first test auf current sensor

--- battery_monitor.ino
+++ battery_monitor.ino
@@ -1,27 +1,16 @@
-#include <Adafruit_INA219.h>
-
 #include <Wire.h>
 #include <Adafruit_INA219.h>
 
+//shunt current sensor
 Adafruit_INA219 ina219;
 
 void setup(void) 
-{
-  uint32_t currentFrequency;
-    
+{  
   Serial.begin(115200);
   Serial.println("Hello!");
   
   // Initialize the INA219.
-  // By default the initialization will use the largest range (32V, 2A).  However
-  // you can call a setCalibration function to change this range (see comments).
   ina219.begin();
-  // To use a slightly lower 32V, 1A range (higher precision on amps):
-  //ina219.setCalibration_32V_1A();
-  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
-  //ina219.setCalibration_16V_400mA();
-
-  Serial.println("Measuring voltage and current with INA219 ...");
 }
 
 void loop(void) 
