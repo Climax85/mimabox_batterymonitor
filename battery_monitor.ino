7ffcd403f40ca91d26638197513b5d2ed664f7b5
Climax85 <climax85@web.de>
2015-10-16 15:15:35 +0200

    simple Ah meter

--- battery_monitor.ino
+++ battery_monitor.ino
@@ -1,33 +1,53 @@
 #include <Wire.h>
 #include <Adafruit_INA219.h>
 
+//Konfiguration
+#define voltagePin       0
+#define enableChargePin  12
+
 //shunt current sensor
 Adafruit_INA219 ina219;
 
-void setup(void) 
-{  
+//initialize variables
+float shuntvoltage   = 0;
+float busvoltage     = 0;
+float current_mA     = 0;
+float loadvoltage    = 0;
+float systemvoltage  = 0;
+float sum_mA         = 0;
+
+//get Volts in mV
+int getVolt() {
+  return (map(analogRead(voltagePin),0,1023,0,18000));
+}
+
+void setup(void)
+{
   Serial.begin(115200);
   Serial.println("Hello!");
   
-  // Initialize the INA219.
+  //Initialize the INA219 - on I2C Bus (A4 & A5)
   ina219.begin();
 }
 
-void loop(void) 
+void loop(void)
 {
-  float shuntvoltage = 0;
-  float busvoltage = 0;
-  float current_mA = 0;
-  float loadvoltage = 0;
-
-  shuntvoltage = ina219.getShuntVoltage_mV();
-  busvoltage = ina219.getBusVoltage_V();
-  current_mA = ina219.getCurrent_mA();
-  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  shuntvoltage  = ina219.getShuntVoltage_mV();
+  busvoltage    = ina219.getBusVoltage_V();
+  loadvoltage   = busvoltage + (shuntvoltage / 1000);
+  systemvoltage = getVolt();
   
-  //print measurements of INA219 to console
-  Serial.print("Load Voltage: "); Serial.print(loadvoltage); Serial.print(" V");
-  Serial.print(" - Current: "); Serial.print(current_mA); Serial.println(" mA");
+  //measure current
+  current_mA    = ina219.getCurrent_mA();
+  //add current to sum
+  sum_mA       += current_mA;
+  
+  //print measurements to Serial
+  Serial.print("System Voltage: "); Serial.print(systemvoltage); Serial.print(" mV");
+  Serial.print(" | Load Voltage: "); Serial.print(loadvoltage); Serial.print(" V");
+  Serial.print(" | Current: "); Serial.print(current_mA); Serial.print(" mA");
+  Serial.print(" | "); Serial.print(sum_mA); Serial.print(" mA");
+  Serial.print(" | "); Serial.print(sum_mA / 3600); Serial.println(" mAh");
 
   //measure every second
   delay(1000);
