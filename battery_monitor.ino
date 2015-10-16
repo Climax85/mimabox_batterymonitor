058c63871d183b67e16d0db549de7409f247e6af
Climax85 <climax85@web.de>
2015-10-16 15:57:20 +0200

    added voltage correction factor for ina219 sensor

--- battery_monitor.ino
+++ battery_monitor.ino
@@ -2,8 +2,8 @@
 #include <Adafruit_INA219.h>
 
 //Konfiguration
-#define voltagePin       0
 #define enableChargePin  12
+#define inaVoltageFactor 1.113
 
 //shunt current sensor
 Adafruit_INA219 ina219;
@@ -15,11 +15,6 @@ float current_mA     = 0;
 float loadvoltage    = 0;
 float systemvoltage  = 0;
 float sum_mA         = 0;
-
-//get Volts in mV
-int getVolt() {
-  return (map(analogRead(voltagePin),0,1023,0,18000));
-}
 
 void setup(void)
 {
@@ -34,8 +29,9 @@ void loop(void)
 {
   shuntvoltage  = ina219.getShuntVoltage_mV();
   busvoltage    = ina219.getBusVoltage_V();
-  loadvoltage   = busvoltage + (shuntvoltage / 1000);
-  systemvoltage = getVolt();
+  
+  //Math for system load voltage with correction factor
+  loadvoltage   = ( busvoltage + (shuntvoltage / 1000) ) * inaVoltageFactor;
   
   //measure current
   current_mA    = ina219.getCurrent_mA();
@@ -43,8 +39,7 @@ void loop(void)
   sum_mA       += current_mA;
   
   //print measurements to Serial
-  Serial.print("System Voltage: "); Serial.print(systemvoltage); Serial.print(" mV");
-  Serial.print(" | Load Voltage: "); Serial.print(loadvoltage); Serial.print(" V");
+  Serial.print("Load Voltage: "); Serial.print(loadvoltage, 4); Serial.print(" V");
   Serial.print(" | Current: "); Serial.print(current_mA); Serial.print(" mA");
   Serial.print(" | "); Serial.print(sum_mA); Serial.print(" mA");
   Serial.print(" | "); Serial.print(sum_mA / 3600); Serial.println(" mAh");
