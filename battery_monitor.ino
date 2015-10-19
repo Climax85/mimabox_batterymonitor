f3c293da2760ad03c052804d380e95cc607cc956
Climax85 <climax85@web.de>
2015-10-19 11:56:54 +0200

    added LCD

--- battery_monitor.ino
+++ battery_monitor.ino
@@ -1,9 +1,20 @@
 #include <Wire.h>
+#include <PCD8544.h>
 #include <Adafruit_INA219.h>
 
 //Konfiguration
 #define enableChargePin  12
 #define inaVoltageFactor 1.113
+
+//LCD Setup
+#define SCLK    12
+#define MOSI    11
+#define DC      10
+#define RST      9
+#define SCE      8
+#define WIDTH   84
+#define HEIGHT  48
+PCD8544 lcd(SCLK, MOSI, DC, RST, SCE);
 
 //shunt current sensor
 Adafruit_INA219 ina219;
@@ -19,7 +30,12 @@ float sum_mA         = 0;
 void setup(void)
 {
   Serial.begin(115200);
-  Serial.println("Hello!");
+  
+  //Initialize the 5110 LCD - Display
+  lcd.begin(WIDTH, HEIGHT);
+  lcd.setContrast(55);
+  lcd.setCursor(0, 0);
+  lcd.print(" Akku Monitor");
   
   //Initialize the INA219 - on I2C Bus (A4 & A5)
   ina219.begin();
@@ -38,7 +54,15 @@ void loop(void)
   //add current to sum
   sum_mA       += current_mA;
   
-  //print measurements to Serial
+  //print to LCD
+  lcd.setCursor(10, 3);
+  lcd.print(loadvoltage, 2); lcd.print(" V");
+  lcd.setCursor(10, 4);
+  lcd.print(current_mA, 1); lcd.print(" mA");
+  lcd.setCursor(10, 5);
+  lcd.print(sum_mA / 3600, 2); lcd.print(" mAh");
+  
+  //print to Serial
   Serial.print("Load Voltage: "); Serial.print(loadvoltage, 4); Serial.print(" V");
   Serial.print(" | Current: "); Serial.print(current_mA); Serial.print(" mA");
   Serial.print(" | "); Serial.print(sum_mA); Serial.print(" mA");
